2f874514b2eb14520d97ee13018cea2eeff5f779
gerardng <gerardngjr@yahoo.com>
2016-08-19 14:27:53 -0700

    contact get

--- contact.php
+++ contact.php
@@ -5,7 +5,7 @@
 		$message = $_POST['message'];
 		$human = intval($_POST['human']);
 		$from = 'Demo Contact Form'; 
-		$to = 'example@bootstrapbay.com'; 
+		$to = 'gerardngjr@yahoo.com'; 
 		$subject = 'Message from Contact Demo ';
 		
 		$body = "From: $name\n E-Mail: $email\n Message:\n $message";
