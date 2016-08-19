3a45b143366b45a20e4f98f33fccd4168f94c60d
gerardng <gerardngjr@yahoo.com>
2016-08-19 14:24:02 -0700

    contact form

--- contact.php
+++ contact.php
@@ -5,10 +5,11 @@
 		$message = $_POST['message'];
 		$human = intval($_POST['human']);
 		$from = 'Demo Contact Form'; 
-		$to = 'example@domain.com'; 
+		$to = 'example@bootstrapbay.com'; 
 		$subject = 'Message from Contact Demo ';
 		
-		$body ="From: $name\n E-Mail: $email\n Message:\n $message";
+		$body = "From: $name\n E-Mail: $email\n Message:\n $message";
+
 		// Check if name has been entered
 		if (!$_POST['name']) {
 			$errName = 'Please enter your name';
@@ -27,12 +28,13 @@
 		if ($human !== 5) {
 			$errHuman = 'Your anti-spam is incorrect';
 		}
+
 // If there are no errors, send the email
 if (!$errName && !$errEmail && !$errMessage && !$errHuman) {
 	if (mail ($to, $subject, $body, $from)) {
 		$result='<div class="alert alert-success">Thank You! I will be in touch</div>';
 	} else {
-		$result='<div class="alert alert-danger">Sorry there was an error sending your message. Please try again later.</div>';
+		$result='<div class="alert alert-danger">Sorry there was an error sending your message. Please try again later</div>';
 	}
 }
 	}
